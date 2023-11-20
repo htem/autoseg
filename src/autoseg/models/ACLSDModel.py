e0e224a409345efdfa3c313a3da6badbda2c1668
brianreicher <reicher.b@northeastern.edu>
2023-11-20 10:47:55 -0500

    Add typing, custom UNet

---
+++ src/autoseg/models/ACLSDModel.py
@@ -0,0 +1,17 @@
+import torch
+
+from ..networks.UNet import UNet, ConvPass
+from ..utils import neighborhood
+
+
+class ACLSDModel(torch.nn.Module):
+    def __init__(self, unet: UNet, num_fmaps: int):
+        super(ACLSDModel, self).__init__()
+
+        self.unet: UNet = unet
+        self.aff_head: ConvPass = ConvPass(num_fmaps, len(neighborhood), [[1, 1, 1]], activation="Sigmoid")
+
+    def forward(self, input):
+        x = self.unet(input)
+        affs = self.aff_head(x)
+        return affs
--- src/autoseg/models/MTLSDModel.py
+++ src/autoseg/models/MTLSDModel.py
@@ -4,19 +4,16 @@ from ..networks.UNet import UNet, ConvPass
 from ..utils import neighborhood
 
 
-class STELARRModel(torch.nn.Module):
-    def __init__(self, unet: UNet, num_fmaps: int) -> None:
-        super(STELARRModel, self).__init__()
+class MTLSDModel(torch.nn.Module):
+    def __init__(self, unet: UNet, num_fmaps: int):
+        super(MTLSDModel, self).__init__()
 
         self.unet: UNet = unet
         self.lsd_head: ConvPass = ConvPass(num_fmaps, 10, [[1, 1, 1]], activation="Sigmoid")
         self.aff_head: ConvPass = ConvPass(num_fmaps, len(neighborhood), [[1, 1, 1]], activation="Sigmoid")
-        self.enhancement_head: ConvPass = ConvPass(num_fmaps, 1, [[1, 1, 1]], activation="Sigmoid")
 
     def forward(self, input):
         x = self.unet(input)
         lsds = self.lsd_head(x[0])
         affs = self.aff_head(x[1])
-        fake = self.enhancement_head(x[2])
-
-        return lsds, affs, fake
+        return lsds, affs
