846058aae1efeea254ee460546ce45214a4238f5
brianreicher <reicher.b@northeastern.edu>
2023-11-20 10:39:30 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# install Rust
+curl https://sh.rustup.rs -sSf | sh
+
+# install MongoDB
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+
+# install graph tools for evaluation
+conda install -c conda-forge -c ostrokach-forge -c pkgw-forge graph-tool
+
+# initialize a MongoDB server
+screen 
+mongod
