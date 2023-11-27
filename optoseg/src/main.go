259308cb9729e68e6b69f0e8af8e53f15a0122a5
brianreicher <reicher.b@northeastern.edu>
2023-11-27 16:00:56 -0500

    Main.go updates

--- optoseg/src/main.go
+++ optoseg/src/main.go
@@ -1,43 +1,18 @@
 package main
 
-import (
-	"fmt"
-	a "optoseg/src/algo/grid"
-	"sync"
-)
+import "autoseg"
 
 func main() {
-	config := a.Config{
-		AdjBiasRange: [2]float64{-1.0, 1.0},
-		LrBiasRange:  [2]float64{-1.0, 1.0},
-		NumSteps:     10,
-	}
+    trainingParams := []struct {
+        modelType   string
+        scriptPath  string
+        args        []string
+    }{
+        {"MTLSD", "./scripts/mtlsd_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--voxel_size", "33"}},
+        {"ACLSD", "./scripts/aclsd_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
+        {"STELARR", "./scripts/stelarr_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
+        // Add more parameter sets as needed
+    }
 
-	// Create a channel to receive scores from goroutines
-	scoresChan := make(chan a.Score, config.NumSteps)
-
-	// Use a WaitGroup to wait for all goroutines to finish
-	var wg sync.WaitGroup
-
-	// Launch goroutines
-	for i := 0; i < config.NumSteps; i++ {
-		wg.Add(1)
-		go a.GridSearch(config, "rand_voi", scoresChan, &wg)
-	}
-
-	// Close the channel when all goroutines are done
-	go func() {
-		wg.Wait()
-		close(scoresChan)
-	}()
-
-	// Collect results from the channel
-	var results []a.Score
-	for score := range scoresChan {
-		results = append(results, score)
-	}
-
-	for _, result := range results {
-		fmt.Printf("ABias: %.2f, LBias: %.2f, Fitness: %.4f\n", result.ABias, result.LBias, result.Fitness)
-	}
+    autoseg.RunTrainingInParallel(trainingParams)
 }
