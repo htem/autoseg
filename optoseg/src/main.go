c631d1a9bd1d35c56b5bd7e8ed5e7642b9422ef8
brianreicher <reicher.b@northeastern.edu>
2023-11-27 16:05:57 -0500

    filename call in main.go

--- optoseg/src/main.go
+++ optoseg/src/main.go
@@ -7,11 +7,10 @@ func main() {
         modelType   string
         scriptPath  string
         args        []string
-    }{
-        {"MTLSD", "./scripts/mtlsd_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--voxel_size", "33"}},
-        {"ACLSD", "./scripts/aclsd_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
-        {"STELARR", "./scripts/stelarr_train.sh", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
-        // Add more parameter sets as needed
+    }{ // TODO: add model type arg
+        {"MTLSD", "../batch_run.py", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--voxel_size", "33"}},
+        {"ACLSD", "../batch_run.py", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
+        {"STELARR", "../batch_run.py", []string{"--iterations", "100000", "--raw_file", "path/to/zarr/or/n5", "--warmup", "100000"}},
     }
 
     autoseg.RunTrainingInParallel(trainingParams)
