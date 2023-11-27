0c01bc983149d86e984768c44925f17ad37732c3
brianreicher <reicher.b@northeastern.edu>
2023-11-27 10:44:42 -0500

    train model func

--- src/autoseg/__init__.py
+++ src/autoseg/__init__.py
@@ -1,2 +1,3 @@
 from . import eval, gp_filters, losses, models, networks, postprocess, predict, train
 from .utils import neighborhood
+from .train_job import train_model
--- src/autoseg/train_job.py
+++ src/autoseg/train_job.py
@@ -1,5 +1,14 @@
-from .train.MTLSDTrain import mtlsd_train
+from .train import mtlsd_train, aclsd_train, stelarr_train
 
-if __name__ == "__main__":
-    mtlsd_train(iterations=10000,
-                data_store="../../../xray-challenge-entry/data/xpress-challenge.zarr")
+    
+def train_model(model_type:str="MTLSD",
+                iterations:int=10000,
+                data_store:str="path/to/zarr/or/n5") -> None:
+    match model_type.lower():
+        case "mtlsd":
+            mtlsd_train(iterations=iterations,
+                        data_store=data_store)
+        case "aclsd":
+            raise NotImplementedError
+        case "stelarr":
+            raise NotImplementedError
