a8bd664e997866da7cbd1db7e66ac7d2b7f5fcb7
brianreicher <reicher.b@northeastern.edu>
2023-11-29 15:52:08 -0500

    Add all train args to train_job

--- src/autoseg/train_job.py
+++ src/autoseg/train_job.py
@@ -5,13 +5,15 @@ def train_model(
     model_type: str = "MTLSD",
     iterations: int = 100000,
     warmup: int = 100000,
-    raw_file: str = "path/to/zarr/or/n5",
+    raw_file: str = "path/to/.zarr/or/.n5",
+    out_file: str = "./raw_predictions.zarr",
     voxel_size: int = 33,
+    save_every=2500,
 ) -> None:
     model_type = model_type.lower()
     if model_type == "mtlsd":
-        mtlsd_train(iterations=iterations, raw_file=raw_file, voxel_size=voxel_size)
+        mtlsd_train(iterations=iterations, raw_file=raw_file, voxel_size=voxel_size, save_every=save_every)
     elif model_type == "aclsd":
-        aclsd_train(iterations=iterations, raw_file=raw_file, warmup=warmup)
+        aclsd_train(iterations=iterations, raw_file=raw_file, out_file=out_file, voxel_size=voxel_size, warmup=warmup, save_every=save_every)
     elif model_type == "stelarr":
-        stelarr_train(iterations=iterations, raw_file=raw_file, warmup=warmup)
+        stelarr_train(iterations=iterations, raw_file=raw_file, out_file=out_file, voxel_size=voxel_size, warmup=warmup, save_every=save_every)
