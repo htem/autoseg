92c499bc7f18706e7f416df85a6ef57633d52be3
brianreicher <reicher.b@northeastern.edu>
2023-12-14 11:06:09 -0500

    GP Filters logs

--- src/autoseg/gp_filters/random_noise.py
+++ src/autoseg/gp_filters/random_noise.py
@@ -3,8 +3,33 @@ import gunpowder as gp
 import random
 from skimage.util import random_noise
 
-
 class RandomNoiseAugment(gp.BatchFilter):
+    """
+    Random Noise Augmentation for Gunpowder.
+
+    This class applies random noise augmentation to the specified array in a Gunpowder batch.
+
+    Args:
+        array (str): 
+            The name of the array in the batch to which noise should be applied.
+        seed (int, optional): 
+            Seed for the random number generator. Default is None.
+        clip (bool, optional): 
+            Whether to clip the values after applying noise. Default is True.
+        **kwargs: 
+            Additional keyword arguments to be passed to the `random_noise` function.
+
+    Attributes:
+        array (str): 
+            The name of the array in the batch to which noise is applied.
+        seed (int): 
+            Seed for the random number generator.
+        clip (bool): 
+            Whether to clip the values after applying noise.
+        kwargs (dict): 
+            Additional keyword arguments passed to the `random_noise` function.
+    """
+
     def __init__(self, array, seed=None, clip=True, **kwargs):
         self.array = array
         self.seed = seed
@@ -12,15 +37,38 @@ class RandomNoiseAugment(gp.BatchFilter):
         self.kwargs = kwargs
 
     def setup(self):
+        """
+        Set up the filter by enabling autoskip and defining array updates.
+        """
         self.enable_autoskip()
         self.updates(self.array, self.spec[self.array])
 
     def prepare(self, request):
+        """
+        Prepare the dependencies for processing based on the requested batch.
+
+        Args:
+            request (BatchRequest): 
+                The requested batch.
+
+        Returns:
+            BatchRequest: 
+                The dependencies for processing.
+        """
         deps = gp.BatchRequest()
         deps[self.array] = request[self.array].copy()
         return deps
 
     def process(self, batch, request):
+        """
+        Apply random noise augmentation to the specified array in the batch.
+
+        Args:
+            batch (Batch): 
+                The input batch.
+            request (BatchRequest): 
+                The requested batch.
+        """
         raw = batch.arrays[self.array]
 
         mode = random.choice(["gaussian", "poisson", "none", "none"])
--- src/autoseg/gp_filters/smooth_array.py
+++ src/autoseg/gp_filters/smooth_array.py
@@ -3,13 +3,39 @@ import gunpowder as gp
 import random
 from scipy.ndimage import gaussian_filter
 
-
 class SmoothArray(gp.BatchFilter):
+    """
+    Smooth Array in a Gunpowder Batch.
+
+    This class applies Gaussian smoothing to a 3D array in a Gunpowder batch.
+
+    Args:
+        array (str): 
+            The name of the array in the batch to be smoothed.
+        blur_range (tuple): 
+            The range of sigma values for the Gaussian filter.
+
+    Attributes:
+        array (str): 
+            The name of the array in the batch to be smoothed.
+        range (tuple): 
+            The range of sigma values for the Gaussian filter.
+    """
+
     def __init__(self, array, blur_range):
         self.array = array
         self.range = blur_range
 
     def process(self, batch, request):
+        """
+        Apply Gaussian smoothing to the specified array in the batch.
+
+        Args:
+            batch (Batch): 
+                The input batch.
+            request (BatchRequest): 
+                The requested batch.
+        """
         array = batch[self.array].data
 
         assert len(array.shape) == 3
